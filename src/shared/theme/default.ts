4d971f898e884129da0198b3a72c5b48f50a1d18
Cheng <chengtan2013@gmail.com>
2020-07-01 17:17:14 -0400

    Update name and logo to common circle

--- src/shared/theme/default.ts
+++ src/shared/theme/default.ts
@@ -20,7 +20,7 @@ export const palette = {
   darkGrey: '#cecece',
   fadedYellow: '#FFF5D9',
 
-  // CommonCircle Exposures color palette
+  // CommonCircle color palette
   brandPurple: '#8D55E9',
   sectionTitle: '#141414',
   secondaryBodyCopy: '#404040',
