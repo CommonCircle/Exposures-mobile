3339f7012efea5acb158132eb3432983c2960c7a
Cheng <chengtan2013@gmail.com>
2020-07-14 14:07:40 -0400

    Add privacy link to onboarding

--- src/screens/onboarding/views/Permissions.tsx
+++ src/screens/onboarding/views/Permissions.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import {ScrollView, StyleSheet} from 'react-native';
+import React, {useCallback} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {ScrollView, StyleSheet, TouchableOpacity, Text as NativeText} from 'react-native';
 import {Box, Text} from 'components';
 import {useI18n} from '@shopify/react-i18n';
 
 export const Permissions = () => {
   const [i18n] = useI18n();
+  const navigation = useNavigation();
+  const onPrivacy = useCallback(() => navigation.navigate('Privacy'), [navigation]);
+
   return (
     <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={styles.content}>
       <Box paddingHorizontal="xl">
@@ -35,6 +39,14 @@ export const Permissions = () => {
             {i18n.translate('OnboardingPermissions.Body3')}
           </Text>
         </Box>
+
+        <Box marginBottom="l">
+          <TouchableOpacity onPress={onPrivacy} style={styles.privacy_button}>
+            <NativeText style={styles.privacy_text}>
+              {i18n.translate('Info.Privacy')}
+            </NativeText>
+          </TouchableOpacity>
+        </Box>
       </Box>
     </ScrollView>
   );
@@ -45,4 +57,13 @@ const styles = StyleSheet.create({
     flexGrow: 1,
     justifyContent: 'center',
   },
+  privacy_button: {
+    alignItems: 'center',
+  },
+  privacy_text: {
+    color: '#5B5B5B',
+    textDecorationLine: "underline",
+    textDecorationStyle: "solid",
+    textDecorationColor: '#5B5B5B'
+  }
 });
